12b63a41f476263c4eb478d88599a9898fd5b47f
Edim J Paul <edimjpaul@gmail.com>
2025-07-10 16:45:48 +0100

    made progress with the aside bar

--- navScroll.js
+++ navScroll.js
@@ -1,5 +1,10 @@
 const navEl = document.querySelector("nav");
 const aLinks = document.querySelectorAll("div.nav-links a");
+const hambuger = document.querySelector("nav svg.hamburger-menu");
+const asideBar = document.querySelector("nav aside");
+const cancelAsideBar = document.querySelector("aside div.as-logo svg");
+const asideOverlay = document.querySelector("aside div.over-lay");
+// console.log(asideBar);
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 70) {
@@ -14,3 +19,17 @@ window.addEventListener("scroll", () => {
     });
   }
 });
+
+//Open asidebar function
+const openaSideBar = () => {
+  asideBar.style.transform = "translateX(0)";
+};
+
+//Close asideBar function
+const closeSideBar = () => {
+  asideBar.style.transform = "translateX(-100vw)";
+};
+
+hambuger.addEventListener("click", openaSideBar);
+cancelAsideBar.addEventListener("click", closeSideBar);
+asideOverlay.addEventListener("click", closeSideBar);
